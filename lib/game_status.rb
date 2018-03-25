dc2f9f4f24264146ccb9157440b79b08ca5e883c
Firat Aksu <aksu_fr@yahoo.es>
2018-03-25 19:53:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,7 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
 
-def WON?(board)
+def won?(board)
   WIN_COMBINATIONS.each do |win_condition|
     if ((win_condition[0] == "X" && win_condition[1] == "X" && win_condition[2] == "X") ||
         (win_condition[0] == "O" && win_condition[1] == "O" && win_condition[2] == "O"))
