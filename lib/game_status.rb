a566415c91204f220fd286596c668546174baea2
Firat Aksu <aksu_fr@yahoo.es>
2018-03-25 19:51:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [5,6,7], [0,3,5], [1,4,6], [2,5,7], [0,4,7], [2,4,5] ]
+WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
 
 def WON?(board)
   WIN_COMBINATIONS.each do |win_condition|
