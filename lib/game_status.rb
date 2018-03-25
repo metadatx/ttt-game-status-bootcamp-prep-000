91a7cb849f4ec518455fb9494ef8e6dccd25e63a
Firat Aksu <aksu_fr@yahoo.es>
2018-03-25 19:57:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,8 +8,8 @@ WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_condition|
-    if ((win_condition[0] == "X" && win_condition[1] == "X" && win_condition[2] == "X") ||
-        (win_condition[0] == "O" && win_condition[1] == "O" && win_condition[2] == "O"))
+    if ((board[win_condition[0]] == "X" && board[win_condition[1]] == "X" && board[win_condition[2]] == "X") ||
+        (board[win_condition[0]] == "O" && board[win_condition[1]] == "O" && board[win_condition[2]] == "O"))
         return win_condition
     end
   end
